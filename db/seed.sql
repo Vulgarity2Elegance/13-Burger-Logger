1d97541155a9940e64c6c1fbf31b3f760874bc5e
Youzhi Hu <jyzhu0101@gmail.com>
2020-08-11 17:37:13 +1000

    Finished DB Setup.

---
+++ db/seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Aussie Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Classic Angus', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Bacon Chesseburger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Big Mac', TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Turkey Burger', TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Whopper', TRUE);
